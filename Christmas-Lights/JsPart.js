97da56ac55265a0ff543e40c9d40a1a1a0472235
parthtyagii <parthtyagicode@gmail.com>
2022-07-11 19:26:05 +0530

    formatting

--- Christmas-Lights/JsPart.js
+++ Christmas-Lights/JsPart.js
@@ -1,61 +1,63 @@
 let Lights = (delay = 0) => {
-        circle1.style.opacity = 1;
-        circle1.style.boxShadow = "30px 0 90px violet, -30px 0 90px violet, 0 30px 90px violet, 0 -30px 90px violet";
+    circle1.style.opacity = 1;
+    circle1.style.boxShadow = "30px 0 90px violet, -30px 0 90px violet, 0 30px 90px violet, 0 -30px 90px violet";
+
+    setTimeout(() => {
+        circle1.style.opacity = .5;
+        circle1.style.boxShadow = "0 0 0 0";
+
+        circle2.style.opacity = 1;
+        circle2.style.boxShadow = "30px 0 90px indigo, -30px 0 90px indigo, 0 30px 90px indigo, 0 -30px 90px indigo";
 
         setTimeout(() => {
-            circle1.style.opacity = .5;
-            circle1.style.boxShadow = "0 0 0 0";
+            circle2.style.opacity = .5;
+            circle2.style.boxShadow = "0 0 0 0";
 
-            circle2.style.opacity = 1;
-            circle2.style.boxShadow = "30px 0 90px indigo, -30px 0 90px indigo, 0 30px 90px indigo, 0 -30px 90px indigo";
-
+            circle3.style.opacity = 1;
+            circle3.style.boxShadow = "30px 0 90px blue, -30px 0 90px blue, 0 30px 90px blue, 0 -30px 90px blue";
             setTimeout(() => {
-                circle2.style.opacity = .5;
-                circle2.style.boxShadow = "0 0 0 0";
+                circle3.style.opacity = .5;
+                circle3.style.boxShadow = "0 0 0 0";
 
-                circle3.style.opacity = 1;
-                circle3.style.boxShadow = "30px 0 90px blue, -30px 0 90px blue, 0 30px 90px blue, 0 -30px 90px blue";
+                circle4.style.opacity = 1;
+                circle4.style.boxShadow = "30px 0 90px green, -30px 0 90px green, 0 30px 90px green, 0 -30px 90px green";
+
                 setTimeout(() => {
-                    circle3.style.opacity = .5;
-                    circle3.style.boxShadow = "0 0 0 0";
+                    circle4.style.opacity = .5;
+                    circle4.style.boxShadow = "0 0 0 0";
 
-                    circle4.style.opacity = 1;
-                    circle4.style.boxShadow = "30px 0 90px green, -30px 0 90px green, 0 30px 90px green, 0 -30px 90px green";
+                    circle5.style.opacity = 1;
+                    circle5.style.boxShadow = "30px 0 90px yellow, -30px 0 90px yellow, 0 30px 90px yellow, 0 -30px 90px yellow";
 
                     setTimeout(() => {
-                        circle4.style.opacity = .5;
-                        circle4.style.boxShadow = "0 0 0 0";
+                        circle5.style.opacity = .5;
+                        circle5.style.boxShadow = "0 0 0 0";
 
-                        circle5.style.opacity = 1;
-                        circle5.style.boxShadow = "30px 0 90px yellow, -30px 0 90px yellow, 0 30px 90px yellow, 0 -30px 90px yellow";
+                        circle6.style.opacity = 1;
+                        circle6.style.boxShadow = "30px 0 90px orange, -30px 0 90px orange, 0 30px 90px orange, 0 -30px 90px orange";
 
                         setTimeout(() => {
-                            circle5.style.opacity = .5;
-                            circle5.style.boxShadow = "0 0 0 0";
+                            circle6.style.opacity = .5;
+                            circle6.style.boxShadow = "0 0 0 0";
 
-                            circle6.style.opacity = 1;
-                            circle6.style.boxShadow = "30px 0 90px orange, -30px 0 90px orange, 0 30px 90px orange, 0 -30px 90px orange";
+                            circle7.style.opacity = 1;
+                            circle7.style.boxShadow = "30px 0 90px red, -30px 0 90px red, 0 30px 90px red, 0 -30px 90px red";
 
                             setTimeout(() => {
-                                circle6.style.opacity = .5;
-                                circle6.style.boxShadow = "0 0 0 0";
+                                circle7.style.opacity = .5;
+                                circle7.style.boxShadow = "0 0 0 0";
 
-                                circle7.style.opacity = 1;
-                                circle7.style.boxShadow = "30px 0 90px red, -30px 0 90px red, 0 30px 90px red, 0 -30px 90px red";
-
-                                setTimeout(() => {
-                                    circle7.style.opacity = .5;
-                                    circle7.style.boxShadow = "0 0 0 0";
-
-                                }, delay);
                             }, delay);
                         }, delay);
                     }, delay);
                 }, delay);
             }, delay);
         }, delay);
+    }, delay);
 };
+// ---------------------------------------------------------------------------------------------------------------
 
+//now...
 let circle1 = document.querySelector("#circle1");
 let circle2 = document.querySelector("#circle2");
 let circle3 = document.querySelector("#circle3");
@@ -66,7 +68,7 @@ let circle7 = document.querySelector("#circle7");
 
 //now...
 let start = document.querySelector("#start");
-start.addEventListener('click',() => {
+start.addEventListener('click', () => {
 
     let delay = document.querySelector('#interval').value;
 
@@ -99,64 +101,3 @@ Stop.addEventListener('click', () => {
     circle7.style.opacity = "50%";
     circle7.style.boxShadow = "0 0 0 0";
 });
-
-// if (document.querySelector("#interval").value >= 1000 && document.querySelector("#interval").value <= 10000) {
-//     delay = document.querySelector("#interval").value;
-// }
-// circle1.style.opacity = 1;
-//         circle1.style.boxShadow = "30px 0 30px violet, -30px 0 30px violet, 0 30px 30px violet, 0 -30px 30px violet";
-
-//         setTimeout(() => {
-//             circle1.style.opacity = .5;
-//             circle1.style.boxShadow = "0 0 0 0";
-
-//             circle2.style.opacity = 1;
-//             circle2.style.boxShadow = "30px 0 30px indigo, -30px 0 30px indigo, 0 30px 30px indigo, 0 -30px 30px indigo";
-
-//             setTimeout(() => {
-//                 circle2.style.opacity = .5;
-//                 circle2.style.boxShadow = "0 0 0 0";
-
-//                 circle3.style.opacity = 1;
-//                 circle3.style.boxShadow = "30px 0 30px blue, -30px 0 30px blue, 0 30px 30px blue, 0 -30px 30px blue";
-
-//                 setTimeout(() => {
-//                     circle3.style.opacity = .5;
-//                     circle3.style.boxShadow = "0 0 0 0";
-
-//                     circle4.style.opacity = 1;
-//                     circle4.style.boxShadow = "30px 0 30px green, -30px 0 30px green, 0 30px 30px green, 0 -30px 30px green";
-
-//                     setTimeout(() => {
-//                         circle4.style.opacity = .5;
-//                         circle4.style.boxShadow = "0 0 0 0";
-
-//                         circle5.style.opacity = 1;
-//                         circle5.style.boxShadow = "30px 0 30px yellow, -30px 0 30px yellow, 0 30px 30px yellow, 0 -30px 30px yellow";
-
-//                         setTimeout(() => {
-//                             circle5.style.opacity = .5;
-//                             circle5.style.boxShadow = "0 0 0 0";
-
-//                             circle6.style.opacity = 1;
-//                             circle6.style.boxShadow = "30px 0 30px orange, -30px 0 30px orange, 0 30px 30px orange, 0 -30px 30px orange";
-
-//                             setTimeout(() => {
-//                                 circle6.style.opacity = .5;
-//                                 circle6.style.boxShadow = "0 0 0 0";
-
-//                                 circle7.style.opacity = 1;
-//                                 circle7.style.boxShadow = "30px 0 30px red, -30px 0 30px red, 0 30px 30px red, 0 -30px 30px red";
-
-//                                 setTimeout(() => {
-//                                     circle7.style.opacity = .5;
-//                                     circle7.style.boxShadow = "0 0 0 0";
-
-//                                 }, delay);
-//                             }, delay);
-//                         }, delay);
-//                     }, delay);
-//                 }, delay);
-//             }, delay);
-//         }, delay);
-
